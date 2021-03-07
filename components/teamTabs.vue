c72bd8bc19a1fe5bfe87a0db396e71d8f47533b1
dlagosb <diegolagosbeltran@gmail.com>
2021-03-07 17:18:07 -0300

    Primer avance de vista teams
    Creación de componentes principales y estructura visual.

---
+++ components/teamTabs.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="teamTabs">
+    <a-tabs default-active-key="1" class="tabPane" size="large">
+      <a-tabPane key="1" tab="ACTIVOS">
+        <ListDisplay
+          title="FC BARCELONA"
+          desc="Matias: Hola chicos, todos listos?"
+          type="none"
+          @click.native="click()"
+        />
+        <ListDisplay
+          title="MAN. UNITED"
+          desc="Franco: Buena Cabros!"
+          @click.native="click()"
+        />
+      </a-tabPane>
+      <a-tabPane key="2" tab="SOLICITUDES">
+        Content of Tab Pane 2
+      </a-tabPane>
+    </a-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+  },
+  methods: {
+    click () {
+      console.log('Probando click de listDisplay para el chat')
+    }
+  }
+}
+</script>
--- pages/teams/index.vue
+++ pages/teams/index.vue
@@ -1,11 +1,45 @@
 <template>
-  <div>
-    Hola
+  <div class="teams">
+    <a-row>
+      <a-col class="leftContent" :span="8">
+        <a-row class="shield">
+          <img src="@/assets/images/shield.svg" alt="">
+        </a-row>
+        <a-row class="options">
+          <TeamTabs />
+          <br>
+          <center>
+            * Sólo puedes pertenecer o crear un máximo de 3 equipos.
+          </center>
+          <br>
+          <a-form-model-item>
+            <PrincipalBtn
+              text="+ CREAR EQUIPO (1/3)"
+              :loading="btnLoading"
+              @click.native="submitForm('ruleForm')"
+            />
+          </a-form-model-item>
+        </a-row>
+      </a-col>
+      <a-col class="rightContent" :span="16">
+        <a-row class="banner">
+          <BannerChat @click.native="click()" />
+        </a-row>
+        <a-row class="chat">
+          COMPONENTE PARA EL CHAT
+        </a-row>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
 <script>
 export default {
-  layout: 'navbar'
+  layout: 'navbar',
+  methods: {
+    click () {
+      console.log('Probando click de BannerChat')
+    }
+  }
 }
 </script>
