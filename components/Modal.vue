ba5d215a44e6b841d174718f2262ddedb949b6f0
dlagosb <diegolagosbeltran@gmail.com>
2021-04-21 19:47:09 -0400

    Quinto avance de vista teams
    Se agrega modal para agregar jugadores

---
+++ components/Modal.vue
@@ -0,0 +1,61 @@
+<template>
+    <a-modal
+        class="geoloc"
+        centered
+        :wrap-class-name="_themePreference === 'light' ? 'lmBody' : 'dmBody'"
+        :mask-closable="false"
+        :footer="false"
+        v-model="value"
+        @cancel="onCancel"
+    >
+        <center>
+            <h1 style="color: white">Agrega un jugador</h1>
+        </center>
+        <br />
+        <h2 style="color: white">
+            Invita a tus amigos o jugadores individuales a ser parte de tu
+            equipo.
+        </h2>
+        <br />
+        <a-row style="display: flex">
+            <PrincipalInput
+                placeholder="Ingresa el email del jugador"
+                style="width: 100%"
+            />
+            <img
+                src="@/assets/icons/dm-search.svg"
+                alt=""
+                style="position: absolute; right: 10px; top: 5px; width: 20px"
+            />
+        </a-row>
+        <br />
+        <a-row>
+            <a-avatar size="large" :src="getImage('')" />
+            <h4 style="color: white">Svenko</h4>
+            <div>
+                <RoundedTextBtn text="solicitar" />
+            </div>
+        </a-row>
+    </a-modal>
+</template>
+
+<script>
+export default {
+    props: {
+        value: { type: Boolean, required: true }
+    },
+    methods: {
+        getImage(image) {
+            if (image === "") {
+                const mode = this._themePreference === "light" ? "lm" : "dm";
+                return require(`@/assets/icons/${mode}-avatar.svg`);
+            } else {
+                return image;
+            }
+        },
+        onCancel() {
+            this.$emit("input", false);
+        }
+    }
+};
+</script>
--- components/infoTeam.vue
+++ components/infoTeam.vue
@@ -1,60 +1,70 @@
 <template>
-  <div class="tabs">
-    <a-tabs default-active-key="1" class="tabPane" size="large">
-      <a-tabPane key="1" :tab="title1">
-        <a-form-model-item>
-          <PrincipalBtn
-            text="+ AGREGAR JUGADOR (3/30)"
-            :loading="btnLoading"
-            @click.native="addPlayer()"
-          />
-        </a-form-model-item>
-        <h3>JUGADORES</h3>
-        <br>
-        <a-row :gutter="[0, 0]" type="flex">
-          <a-col
-            v-for="k in require('./../static/data/players.json')"
-            :key="`r${k.name}`"
-            :span="24"
-          >
-            <ListBtn
-              :key="`l${k.name}`"
-              :desc="k.name"
-              :picture1="k.img"
-              :value="k.name"
-              @click.native="click()"
-            />
-          </a-col>
-        </a-row>
-      </a-tabPane>
-      <a-tabPane key="2" :tab="title2">
-        <img src="https://image-api.onlineeducation.center/v2/image/max-width/800/blog/4_3_3.jpg" alt=""
-        style="width: 100%;"
-        >
-      </a-tabPane>
-    </a-tabs>
-  </div>
+    <div class="tabs">
+        <a-tabs default-active-key="1" class="tabPane" size="large">
+            <a-tabPane key="1" :tab="title1">
+                <a-form-model-item>
+                    <PrincipalBtn
+                        text="+ AGREGAR JUGADOR (3/30)"
+                        :loading="btnLoading"
+                        @click.native="toggleModal()"
+                    />
+                </a-form-model-item>
+                <Modal v-model="visible" />
+                <h3>JUGADORES</h3>
+                <br />
+                <a-row :gutter="[0, 0]" type="flex">
+                    <a-col
+                        v-for="k in require('./../static/data/players.json')"
+                        :key="`r${k.name}`"
+                        :span="24"
+                    >
+                        <ListBtn
+                            :key="`l${k.name}`"
+                            :desc="k.name"
+                            :picture1="k.img"
+                            :value="k.name"
+                            @click.native="click()"
+                        />
+                    </a-col>
+                </a-row>
+            </a-tabPane>
+            <a-tabPane key="2" :tab="title2">
+                <img
+                    src="https://image-api.onlineeducation.center/v2/image/max-width/800/blog/4_3_3.jpg"
+                    alt=""
+                    style="width: 100%"
+                />
+            </a-tabPane>
+        </a-tabs>
+    </div>
 </template>
 
 <script>
 export default {
-  props: {
-    title1: {
-      type: String,
-      default: 'Tab1'
+    props: {
+        title1: {
+            type: String,
+            default: "Tab1"
+        },
+        title2: {
+            type: String,
+            default: "Tab2"
+        }
     },
-    title2: {
-      type: String,
-      default: 'Tab2'
-    }
-  },
-  methods: {
-    click () {
-      console.log('Probando click de listDisplay para el chat')
+    data() {
+        return {
+            loading: false,
+            visible: false
+        };
     },
-    addPlayer () {
-      console.log('Probando click para agregar jugadores')
+    methods: {
+        click() {
+            console.log("Probando click de listDisplay para el chat");
+        },
+        toggleModal() {
+            console.log("Probando click para agregar jugadores");
+            this.visible = !this.visible;
+        }
     }
-  }
-}
+};
 </script>
